71c0594041858f976c71fa574492021ff355b33c
dpjoshua <pauljoshua.devarapalli@gmail.com>
2024-01-07 15:52:59 -0500

    Deployed website

--- src/config/firedb.js
+++ src/config/firedb.js
@@ -5,14 +5,14 @@ import { getDatabase } from 'firebase/database';
 function startFirebase(){  
   const firebaseConfig = {
     // Your Credentials
-    apiKey: "AIzaSyC-keomUXUClwMExZJgtrzPp39V4-p0wqQ",
-    authDomain: "fauth-4be60.firebaseapp.com",
-    databaseURL: "https://fauth-4be60-default-rtdb.firebaseio.com",
-    projectId: "fauth-4be60",
-    storageBucket: "fauth-4be60.appspot.com",
-    messagingSenderId: "96361812486",
-    appId: "1:96361812486:web:66f61a7cc4bb060c10a987",
-    measurementId: "G-W70ZDGPQC5"
+    apiKey: "AIzaSyD6BjPrAHgknLvHHZtDBgNmHOJuC7IEu-Q",
+  authDomain: "faproject-bc874.firebaseapp.com",
+  databaseURL: "https://faproject-bc874-default-rtdb.firebaseio.com",
+  projectId: "faproject-bc874",
+  storageBucket: "faproject-bc874.appspot.com",
+  messagingSenderId: "755901330586",
+  appId: "1:755901330586:web:bc6da855d8f01669c39b23",
+  measurementId: "G-WQ01X50JJF"
   };
   const app = initializeApp(firebaseConfig)
   return getDatabase(app);
